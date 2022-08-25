00f68c4ddccb8f08cbc5fe8604a3dcab849bb844
Stefan Holodnick <stefan@dailyinvention.com>
2022-08-25 00:33:29 -0400

    main-edit: changed to error

--- .github/scripts/sdf-deploy.sh
+++ .github/scripts/sdf-deploy.sh
@@ -3,7 +3,7 @@
 # setup Netsuite authentication
 suitecloud account:savetoken --account $3 --authid wd-prod-ci --tokenid $1 --tokensecret $2
 
-suitecloud project:deploy --accountspecificvalues WARNING
+suitecloud project:deploy --accountspecificvalues ERROR
 
 #remove credentials
 suitecloud account:manageauth --remove wd-prod-ci
